128ec43932ff4c1507fa28f55c4b32b1a5cd050a
sebsto93 <s.stobiecki93@gmail.com>
2024-08-30 18:49:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Phonebook</h1>
     </>
   )
 }
