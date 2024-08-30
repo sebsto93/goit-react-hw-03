ee85010d7970450a388c2b73f2a815ee735030f5
sebsto93 <s.stobiecki93@gmail.com>
2024-08-30 20:38:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,28 @@ import SearchBox from './components/SearchBox'
 import { nanoid } from 'nanoid'
 import styles from './App.module.css'
 
-const App = () => {
-  const initialContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+]
 
-  const [contacts, setContacts] = useState(initialContacts)
+const App = () => {
+  const [contacts, setContacts] = useState([])
   const [filter, setFilter] = useState('')
 
+
   useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem('contacts')) || []
-    if (savedContacts.length === 0) {
-      setContacts(initialContacts)
-    } else {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'))
+    if (savedContacts && savedContacts.length > 0) {
       setContacts(savedContacts)
+    } else {
+ 
+      setContacts(initialContacts)
     }
   }, [])
+
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts))
@@ -31,11 +34,19 @@ const App = () => {
 
   const addContact = (name, number) => {
     const newContact = { id: nanoid(), name, number }
-    setContacts([...contacts, newContact])
+    setContacts(prevContacts => {
+      const updatedContacts = [...prevContacts, newContact]
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts))
+      return updatedContacts
+    })
   }
 
   const deleteContact = (id) => {
-    setContacts(contacts.filter(contact => contact.id !== id))
+    setContacts(prevContacts => {
+      const updatedContacts = prevContacts.filter(contact => contact.id !== id)
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts))
+      return updatedContacts
+    })
   }
 
   const handleFilterChange = (e) => {
